4af77e8465393a7997f60b0c679a9041fa7217d1
Kananek T <info.dvgamer@gmail.com>
2018-12-15 23:46:49 +0700

    project init

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  corntab: require('./lib/crontab')
+}
---
+++ test/index.js
@@ -0,0 +1,70 @@
+const { expect, assert } = require('chai')
+
+describe('node_debuger', () => {
+  const main = require('../index')
+  // let scope1
+  // let scope2
+  // let connClose
+
+  // before(async () => {
+  //   scope1 = main.debuger
+  //   scope2 = main.debuger.scope('scope2')
+  // })
+
+  // describe('#logger()', () => {
+  //   it('message', async () => {
+  //     scope1.start('start message.')
+  //     scope1.log('log message.')
+  //     scope1.info('info message.')
+  //     scope1.error(new Error('error message.'))
+  //     scope1.success('success message.')
+  //   })
+
+  //   it('message with scope', async () => {
+  //     scope2.start('start message.')
+  //     scope2.log('logmessage.')
+  //     scope2.info('info message.')
+  //     scope2.error(new Error('error message.'))
+  //     scope2.success('success message.')
+  //   })
+  // })
+
+  // describe('#helper()', () => {
+  //   // it('Functional -- raven', done => {
+  //   //   const Raven = require('../helper/raven')
+  //   //   Raven.install({}, 'unit-testing')
+  //   //   Raven.warning('test message')
+  //   //   Raven.error(new Error('Testing Error.'))
+  //   //   Raven.ProcessClosed(process, async () => {})
+  //   //   Raven.Tracking(async () => {
+  //   //     done()
+  //   //   })
+  //   // })
+  //   it('class time.seconds', done => {
+  //     let elapsed = new main.Time()
+  //     assert(typeof elapsed.seconds, 'function')
+  //   })
+  //   it('class time.nanoseconds', done => {
+  //     let elapsed = new main.Time()
+  //     expect(elapsed).to.have.property('nanoseconds')
+  //   })
+  //   it('class time.total', done => {
+  //     let elapsed = new main.Time()
+  //     expect(elapsed.seconds).to.have.property('total')
+  //   })
+  //   it('const DevMode', done => {
+  //     expect(main).to.have.property('DevMode')
+  //   })
+  //   it('const DebugMode', done => {
+  //     expect(main).to.have.property('DebugMode')
+  //   })
+  //   it('const IsLinux', done => {
+  //     expect(main).to.have.property('IsLinux')
+  //   })
+  //   it('const IsWindows', done => {
+  //     expect(main).to.have.property('IsWindows')
+  //   })
+  // })
+  
+
+})
